64a1151cf44592ef587bb0c05a15d998615db9a0
Ionică Bizău <bizauionica@gmail.com>
2020-04-10 14:59:15 -0500

    :arrow_up: 2.0.0 :tada:

--- test/index.js
+++ test/index.js
@@ -8,16 +8,16 @@ const tester = require("tester")
 tester.describe("showdown-emoji", t => {
     t.should("A Showdown extension for emoji the code blocks.", () => {
         let converter = new showdown.Converter({
-            extensions: [showdownEmoji]
+            extensions: [showdownEmoji("/path/to/emojis")]
         });
         let html = converter.makeHtml(`Hello World! :heart:`);
-        t.expect(html).toBe(`<p>Hello World! <img src="https://github.global.ssl.fastly.net/images/icons/emoji/heart.png?v5" alt=":heart:" title=":heart:" class="emoji-img emoji"></p>`);
+        t.expect(html).toBe(`<p>Hello World! <img src="/path/to/emojis/heart.png?v5" alt=":heart:" title=":heart:" class="emoji-img emoji"></p>`);
     });
     t.should("not replace :everything:", () => {
         let converter = new showdown.Converter({
-            extensions: [showdownEmoji]
+            extensions: [showdownEmoji("/path/to/emojis")]
         });
         let html = converter.makeHtml(`Hello World! :heart: :this: won't get replaced`);
-        t.expect(html).toBe(`<p>Hello World! <img src="https://github.global.ssl.fastly.net/images/icons/emoji/heart.png?v5" alt=":heart:" title=":heart:" class="emoji-img emoji"> :this: won't get replaced</p>`);
+        t.expect(html).toBe(`<p>Hello World! <img src="/path/to/emojis/heart.png?v5" alt=":heart:" title=":heart:" class="emoji-img emoji"> :this: won't get replaced</p>`);
     });
 });
